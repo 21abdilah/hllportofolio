7807d2f56a3f09ec047ce02e68808c00fc0b5ad5
21abdilah <158019652+21abdilah@users.noreply.github.com>
2025-10-06 21:27:51 +0700

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar">
+    <div class="container inner">
+      <div class="brand">{{ shortBrand }}</div>
+      <div class="nav-links">
+        <a href="#about">Tentang</a>
+        <a href="#skills">Keahlian</a>
+        <a href="#achievements">Pencapaian</a>
+        <a href="#gallery">Galeri</a>
+        <a href="#plans">Rencana</a>
+        <a href="#contact">Kontak</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { profile } from '../data.js'
+const shortBrand = profile.name.split(' ')[0] + " • INFORMATIKA"
+</script>
