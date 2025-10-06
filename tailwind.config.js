30cb5e11b4b8f3e016d698e5f290b0cf451782c8
21abdilah <158019652+21abdilah@users.noreply.github.com>
2025-10-06 20:16:00 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-p
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      backgroundImage: {
+        'tech-pattern': 'radial-gradient(circle at 1px 1px, rgba(255,255,255,0.1) 1px, transparent 0)',
+      },
+    },
+  },
+  plugins: [],
+}
