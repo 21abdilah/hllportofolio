c8d1be95376d6150e30206f64c08c710ea2d5fd6
21abdilah <158019652+21abdilah@users.noreply.github.com>
2025-10-06 20:19:22 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,1 +1,33 @@
-p
+export const profile = {
+  name: "Hilal Abdilah",
+  program: "Teknik Informatika",
+  university: "STIMIK DCI Tasikmalaya",
+  interest: "Pemrograman, Desain Digital, dan AI",
+  about: "Mahasiswa baru angkatan 2k25. Saya percaya informatika adalah kunci untuk menciptakan solusi cerdas, inovatif, dan berdampak positif bagi masyarakat."
+};
+
+export const skills = [
+  "Dasar Pemrograman (HTML, CSS, JavaScript)",
+  "Desain Grafis (Canva, Figma)",
+  "UI/UX Dasar",
+  "Editing Video (CapCut)",
+
+];
+
+export const achievements = [
+  "Mengikuti Pelatihan Dasar Web Development Online",
+  "Membuat aplikasi sederhana menggunakan HTML & JavaScript",
+  "Lulus dari SMK"
+];
+
+export const gallery = [
+  { title: "Desain web", img: "/poster1.jpg" },
+  { title: "Sertfikat lcc", img: "/sertifikat1.jpg" },
+  { title: "Kegiatan coding", img: "/kegiatan1.jpg" }
+];
+
+export const plans = {
+  smart: "Memanfaatkan platform online seperti Dicoding, Coursera, dan YouTube untuk memperdalam ilmu pemrograman.",
+  creative: "Membuat karya digital inovatif seperti web interaktif, desain UI, dan mini-project open source.",
+  competitive: "Membangun personal branding melalui portofolio online."
+};
